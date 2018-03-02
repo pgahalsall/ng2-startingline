89aff470b16c82b8ae24f2f53168f8a980d80c3a
ANNAPOLISPC\Pete <peter.halsall@gmail.com>
2018-03-02 16:41:18 -0500

    added build ci

--- gulpfile.js
+++ gulpfile.js
@@ -13,29 +13,35 @@ gulp.task('clean', function (cb) {
 /**
  * Copy all resources that are not TypeScript files into build directory.
  */
-gulp.task("resources", ["server", "app", "assets"], function () {
-    console.log("Building resources...");
+//gulp.task("resources", ["server", "app", "assets"], function () {
+gulp.task("resources", ["server"], function () {
+    // console.log("Building resources...");
+    return gulp.src(["src/**/*", "!**/*.ts", "!src/server", "!src/server/**"])
+             .pipe(gulp.dest("build"));
 });
 /* copy the app core files to the build folder */
-gulp.task("app", ['index'], function(){
-    return gulp.src(["app/**", "!app/**/*.ts"])
-        .pipe(gulp.dest("build/app"));
-});
-/* get the index file to the root of the build */
+// gulp.task("app", ['index'], function(){
+//     return gulp.src(["src/app/**", "!src/app/**/*.ts"])
+//         .pipe(gulp.dest("build/app"));
+// });
+
+/* get the index file to the root of the build 
 gulp.task("index", function(){
     return gulp.src(["index.html"])
         .pipe(gulp.dest("build"));
-});
+});*/
+
 /* copy node server to build folder */
 gulp.task("server", function () {
-    return gulp.src(["index.js", "package.json"], { cwd: "server/**" })
+    return gulp.src(["index.js", "package.json"], { cwd: "src/server/**" })
         .pipe(gulp.dest("build"));
 });
-/* styles and other assets */
+/* styles and other assets 
 gulp.task("assets", function(){
     return gulp.src(["styles.css"])
         .pipe(gulp.dest("build"));
-});
+});*/
+
 /**
  * Copy all required libraries into build directory.
  */
@@ -50,7 +56,7 @@ gulp.task("libs", function () {
         'angular2/bundles/angular2.dev.js',
         'angular2/bundles/router.dev.js'
     ], { cwd: "node_modules/**" }) /* Glob required here. */
-        .pipe(gulp.dest("build/node_modules"));
+        .pipe(gulp.dest("build/lib"));
 });
 /**
  * Build the project.
